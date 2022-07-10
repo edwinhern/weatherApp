460f612ba79aa9e1f163bb02f6dd509c8d5d7b0c
Edwin Hernandez <Edwinhernandez2@my.unt.edu>
2022-07-09 23:37:40 -0500

    created the code structure for the project and made a homeScreen file

---
+++ animated-weatherapp/src/screens/main.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Text,Box, Center } from 'native-base';
+
+export default function MainScreen() {
+    return (
+        <Center _dark={{ bg: 'blueGray.900'}} _light={{bg: 'blueGray.50'}} px={4} flex={1}>
+            <Box>
+                <Text>This is main.tsx</Text>
+            </Box>
+        </Center>
+    )
+}
