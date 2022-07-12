c0de3210a1724c5f3f26a244b9698eef6aa53e8e
Edwin Hernandez <Edwinhernandez2@my.unt.edu>
2022-07-11 21:56:35 -0500

    created home and about screen. Worked on the Navbar and masthead section

--- animated-weatherapp/src/screens/main.tsx
+++ animated-weatherapp/src/screens/main.tsx
@@ -1,12 +1,36 @@
-import * as React from 'react';
-import { Text,Box, Center } from 'native-base';
-
+import * as React from "react";
+import { Text, Box, Center, VStack, useColorModeValue } from "native-base";
+import ThemeToggle from "../components/theme-toggle";
+import Masthead from "../components/masthead";
+import Navbar from "../components/navbar";
+import AnimatedColorBox from '../components/animate-color-box'
 export default function MainScreen() {
-    return (
-        <Center _dark={{ bg: 'blueGray.900'}} _light={{bg: 'blueGray.50'}} px={4} flex={1}>
-            <Box>
-                <Text>This is main.tsx</Text>
-            </Box>
-        </Center>
-    )
+  return (
+    <AnimatedColorBox
+      flex={1}
+      bg={useColorModeValue('warmGray.50', 'primary.900')}
+      w="full"
+    >
+      <Masthead
+        title="What's up, Edwin!"
+        image={require("../assets/masthead.png")}
+      >
+        <Navbar />
+      </Masthead>
+      <VStack 
+        flex={1} 
+        space={5} 
+        alignItems="center"
+        bg={useColorModeValue('warmGray.50', 'primary.900')}
+        mt="-20px"
+        borderTopLeftRadius="20px"
+        borderTopRightRadius="20px"
+        pt="20px">
+        <Box p={10} bg={useColorModeValue("red.500", "yellow.500")}>
+          <Text>This is main.tsx</Text>
+        </Box>
+        <ThemeToggle />
+      </VStack>
+    </AnimatedColorBox>
+  );
 }
