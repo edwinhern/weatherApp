b69f4501d108bb6b7a6956ccec94af2c40d92fd2
Edwin Hernandez <Edwinhernandez2@my.unt.edu>
2022-07-11 21:58:44 -0500

    created home and about screen. Worked on the Navbar and masthead section (#1)

--- animated-weatherapp/App.tsx
+++ animated-weatherapp/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import AppContainer from "./src/components/app-container";
 import Main from "./src/screens/main";
+import Navigator from "./src/index";
+import 'react-native-gesture-handler'
 
 export default function App() {
   return (
     <AppContainer>
-      <Main />
+      <Navigator />
     </AppContainer>
   );
 }
---
+++ animated-weatherapp/src/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import MainScreen from "./screens/main";
+import AboutScreen from "./screens/about-screen";
+const Drawer = createDrawerNavigator();
+
+const App = () => {
+  return (
+    <Drawer.Navigator
+      initialRouteName="Main"
+      screenOptions={{
+        headerShown: false,
+        drawerType: "back",
+        overlayColor: "#00000000",
+      }}
+    >
+      <Drawer.Screen name="Main" component={MainScreen} />
+      <Drawer.Screen name="About" component={AboutScreen} />
+    </Drawer.Navigator>
+  );
+};
+
+export default App;
